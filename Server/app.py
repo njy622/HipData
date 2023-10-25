7a32161130f334360d8a55e94b5cfdb88341912e
njy622 <njy622@naver.com>
2023-10-25 10:39:49 +0900

    2023.10.25

---
+++ Server/app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, flash, session
+from bp.crawling import crawl_bp
+from bp.map import map_bp
+from bp.user import user_bp
+from bp.chatbot import chatbot_bp
+from bp.schedule import schdedule_bp
+import os, json, random
+import util.map_util as mu
+import util.weather_util as wu
+import util.image_util as iu
+import db_sqlite.profile_dao as pdao
+
+app = Flask(__name__)
+app.secret_key = 'qwert12345'       # flash와 session을 사용하려면 반드시 설정해야 함
+app.config['SESSION_COOKIE_PATH'] = '/'
+
+app.register_blueprint(crawl_bp, url_prefix='/crawling')    # localhost:5000/crawling/* 는 crawl bp가 처리
+app.register_blueprint(map_bp, url_prefix='/map')
+app.register_blueprint(user_bp, url_prefix='/user')
+app.register_blueprint(chatbot_bp, url_prefix='/chatbot')
+app.register_blueprint(schdedule_bp, url_prefix='/schedule')
+
+# for AJAX ###################################################
+@app.route('/weather')
+def weather():
+    # 서울시 영등포구 + '청' -> 도로명 주소 -> 카카오 로컬 -> 좌표 획득
+    addr = request.args.get('addr') 
+    lat, lng = mu.get_coord(app.static_folder, addr + '청')
+    html = wu.get_weather(app.static_folder, lat, lng)
+    return html
+
+@app.route('/changeProfile', methods=['GET','POST'])
+def change_profile():
+    if request.method == 'GET':
+        profile = pdao.get_profile(session['profile'][0])
+        return json.dumps(profile)
+    else:
+        email = request.form['email']
+        try:
+            file_image = request.files['image']
+            image = file_image.filename
+            filename = os.path.join(app.static_folder, f'upload/{file_image.filename}')
+            file_image.save(filename)
+            mtime = iu.change_profile(app.static_folder, filename, session['uid'])
+        except:
+            image = request.form['hiddenImage']     # image의 변화가 없으면 hiddenImage값을 사용
+            mtime = 0
+        state_msg = request.form['stateMsg']
+        github = request.form['github']
+        insta = request.form['insta']
+        addr = request.form['addr']
+        params = [image, state_msg, github, insta, addr, email]
+        pdao.update_profile(params)
+        # github, insta, addr 값이 추가되면 need_refresh 값을 1로 세팅 --> 윈도우 Reload 하게 함
+        need_refresh = 0 if session['profile'][3] and session['profile'][4] and session['profile'][5] else 1
+        profile = [email, image, state_msg, github, insta, addr]
+        session['profile'] = profile
+        profile.append(session['uid'])
+        profile.append(mtime)
+        profile.append(need_refresh)
+        return json.dumps(profile)
+###################################################
+
+@app.route('/')
+def home():
+    menu = {'ho':1, 'us':0, 'cr':0, 'ma':0, 'cb':0, 'sc':0}
+    # flash('Welcome to my Web!!!')
+    return render_template('home.html', menu=menu)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Server/util/receipt_util.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+receipt_data = '''
+이마트 청계천절(0212290-1234
+06030 【  208-80-60013 이갑수
+서울시 종구 청계천로 400
+영수증 미지참시 교환/환불 불가(30일내)
+환불 구매점에서 가능(결제키느시잠)
+체크키드 취소 시 대금환급은 최대 7일소요
+
+2014206004 11113       60005
+
+미 (0)치킨 너켓 19      10,500
+02 용가리치킨520+140      8.550
+03 대림 안심지킨너540  7,980
+04 치리 기슴살치킨너켓  5,990
+수실류-호두
+호두
+너겟
+가슬살
+치킨
+자몽
+
+7.80
+
+부기세 면세 품목입니다.
+
+0088.000
+'''
+
+def receipt_get_point(receipt_data):
+    # 영수증 전처리
+    receipt = receipt_data.split('\n')
+    receipt_df = pd.DataFrame(receipt, columns=['title'])
+    receipt_df['title'] = receipt_df['title'].apply(lambda x: re.sub('[^가-힣]', '', x))
+    receipt_df = receipt_df[receipt_df.title != '']
+    receipt_df.set_index('title', inplace=True)
+    receipt_df.reset_index(inplace=True)
+
+    # 비교 데이터프레임 호출
+    list_df = pd.read_csv('data/eco_product.csv')
+
+    # TF-IDF 벡터화
+    tv = TfidfVectorizer()
+    receipt_tv = tv.fit_transform(receipt_df['title'])
+    list_tv = tv.transform(list_df['title_mod'])
+
+    # 코사인 유사도 계산
+    cosine_similarities = cosine_similarity(receipt_tv, list_tv)
+
+    # 유사도 컬럼으로 추가
+    receipt_df['cosine_similarity'] = cosine_similarities.max(axis=1)
+
+    # 유사한 항목 추가
+    most_similar_indices = cosine_similarities.argmax(axis=1)
+    most_similar_items = list_df.loc[most_similar_indices, 'title_mod'].values
+    receipt_df['most_similar_item'] = most_similar_items
+    receipt_df['point'] = list_df.loc[most_similar_indices, 'point'].values
+    receipt_df['title'] = list_df.loc[most_similar_indices, 'title'].values
+
+    # cosine_similarity의 값이 0.5 이상인 경우만 추출
+    receipt_df = receipt_df[receipt_df['cosine_similarity'] >= 0.5]
+
+    # 포인트 합계
+    return receipt_df['point'].sum()
+
+    # 비교 데이터프레임 호출
+    list_df = pd.read_csv('data/eco_product.csv')
+
+    # 검색어 데이터프레임 화
+    title = [title]
+    title_df = pd.DataFrame(title, columns=['title'])
+    title_df
+
+    # TF-IDF 벡터화
+    tv = TfidfVectorizer()
+    title_tv = tv.fit_transform(title_df['title'])
+    list_tv = tv.transform(list_df['title'])
+
+    # 코사인 유사도 계산
+    cosine_similarities = cosine_similarity(title_tv, list_tv)
+
+    # 유사도 컬럼으로 추가
+    list_df['cosine_similarity'] = cosine_similarities.reshape(-1,1)
+
+    # cosine_similarity의 값이 0.5 이상인 경우만 추출
+    list_df = list_df[list_df['cosine_similarity'] >= 0.3]
+
+    # 관련된 마켓명
+    market = set(list_df['market'].values.tolist())
+    return list(market)[:5]
+
